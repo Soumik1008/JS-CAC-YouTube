b3bcaae540401c8152bf4851eb81b365a2e00275
Soumik Saha <83770214+Soumik1008@users.noreply.github.com>
2024-12-06 10:04:00 +0000

    Container Config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 37997;
+let accountEmail = "soumik.s@tataelxsi.co.in";
+var accountPassword = "December@2412";
+accountCity = "Trivandrum";
+let accountState;
+
+// accountId = 40404;
+
+
+/*
+    Prefer not to use var because of Block Scope and Functional Scope
+*/
+accountEmail = "s.s@tel";
+accountPassword = "21212121";
+accountCity = "Bengaluru";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// alert(3+3);
+
+let name = "Ashish";
+let age = 24;
+let isloggedIn = false;
+let state;
+
+console.log(typeof null) //object
+console.log(typeof undefined); // undefined
+console.log("Hitesh");
